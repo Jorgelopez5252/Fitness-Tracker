abf9791aac14ca7014b8d6a8bc0b2ea61f04e6f2
Jorge Lopez <JorgeLopez5252@gmail.com>
2019-12-12 21:25:17 -0700

    updated

--- public/js/app.js
+++ public/js/app.js
@@ -1,46 +1,65 @@
+$(document).ready(function() {
+  $("#submit").click(function() {
+    event.preventDefault();
 
+    const duration = parseInt(
+        $("#duration")
+          .val()
+          .trim()
+      );
+      if(isNaN(duration)){
+          alert("Hey add a duration")
+          return;
+      }
 
-$(document).ready(function(){
-    $("#submit").click(function() {
-        event.preventDefault();
+    const workoutInput = {
+      type: $("#exercise-type")
+        .val()
+        .trim(),
+      name: $("#exercise-name")
+        .val()
+        .trim(),
+      duration
+    };
 
-        const workoutInput = {
-            type:  $("#exercise-type").val().trim(),
-            name: $("#exercise-name").val().trim(),
-            duration: parseInt( $("#duration").val().trim())
-        }
+    console.log(workoutInput);
 
-
-        console.log(workoutInput);
-
-        $.ajax("/api/workout", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            data: JSON.stringify(workoutInput)
-        }).then(function () {
-            console.log("new workout added");
-        });
+    $.ajax("/api/workout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      data: JSON.stringify(workoutInput)
+    }).then(function() {
+      console.log("new workout added");
+      getExercise();
     });
+  });
 
-function getExercise () {
-//ajax call with a get route//
+  function getExercise() {
+    //ajax call with a get route//
 
-$.ajax("/all", {
-    method: "GET",
-}).then(function (result) {
-console.log(result);
-//get values off of result and append to table 
+    $.ajax("/all", {
+      method: "GET"
+    }).then(function(result) {
+      console.log(result);
+      //get values off of result and append to table
+      $("#emptyDiv").empty();
+      result.forEach(function(workout){
+        var newExercise = $(
+          `<div class="col exercise-col">
+            <span id="">Exercise name:${workout.name}</span>
+            <span id="">Excercise Type: ${workout.type}</span>
+            <span id="">Duration: ${workout.duration}</span>
+            </div>`
+        );
+        $("#emptyDiv").append(newExercise);
+      })
+        
+      
 
 
+      
+    });
+  }
 
-
-
-
-});
-    
-    
-};
-
-getExercise();
-
+  getExercise();
 });
